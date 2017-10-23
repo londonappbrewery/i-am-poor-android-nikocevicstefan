ce64f8d89726ffea864859fe450eb11c5eaa2bdf
nikocevicstefan <31037015+nikocevicstefan@users.noreply.github.com>
2017-10-23 16:07:36 +0200

    Challenge completed!

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -2,7 +2,10 @@ package com.londonappbrewery.iampoor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
